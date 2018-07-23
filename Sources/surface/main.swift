9d0519ceaa010745d20fd62aa77ff6e049a95df3
Leandro Ostera <leandro@ostera.io>
2018-07-23 09:03:25 +0200

    ~[daemon] Reformat

--- Sources/surface/main.swift
+++ Sources/surface/main.swift
@@ -1,17 +1,17 @@
+import Cocoa
 import IOKit
 import IOKit.usb
 import IOKit.usb.IOUSBLib
 import USBDeviceSwift
-import Cocoa
 
 enum EXIIReport: UInt8 {
-    case CoordinateData=1   // Get Raw and Adjusted Coordinates from the device
-    case Calibrate=4        // Calibrate device
-    case ControllerStatus=6 // Get the controller status
-    case Reset=7            // Reset the device!
-    case RestoreDefaults=8
-    case ControllerID=10
-    case ReadParameter=16
+    case CoordinateData = 1 // Get Raw and Adjusted Coordinates from the device
+    case Calibrate = 4 // Calibrate device
+    case ControllerStatus = 6 // Get the controller status
+    case Reset = 7 // Reset the device!
+    case RestoreDefaults = 8
+    case ControllerID = 10
+    case ReadParameter = 16
 }
 
 enum EXIIReqType: UInt8 {
@@ -20,13 +20,13 @@ enum EXIIReqType: UInt8 {
 }
 
 enum EXIICalibrationMode: UInt16 {
-    case Extended=1
-    case Corner=2
+    case Extended = 1
+    case Corner = 2
 }
 
 enum EXIIResetMode: UInt16 {
-    case SoftReset=1
-    case HardReset=2
+    case SoftReset = 1
+    case HardReset = 2
 }
 
 enum EXIIDeviceError: Error {
@@ -34,7 +34,7 @@ enum EXIIDeviceError: Error {
     case RequestError(desc: String)
 }
 
-enum EXIIPowerOnCheckStatus : UInt8 {
+enum EXIIPowerOnCheckStatus: UInt8 {
     case RAMDataError = 0
     case RAMCodeError = 1
     case StrayError = 2
@@ -65,61 +65,65 @@ enum EXIICommandStatus: UInt8 {
 }
 
 struct EXIIDeviceStatus {
-    public var reportId : EXIIReport
-    public var powerOnCheckStatus : EXIIPowerOnCheckStatus
-    public var lastCommandStatus : EXIICommandStatus
-    public var touchStatus : UInt8
-    public var asyncReports : UInt16
+    public var reportId: EXIIReport
+    public var powerOnCheckStatus: EXIIPowerOnCheckStatus
+    public var lastCommandStatus: EXIICommandStatus
+    public var touchStatus: UInt8
+    public var asyncReports: UInt16
 }
 
 class EXIIDevice {
     var deviceInfo: USBDevice
-    
-    required init(_ deviceInfo:USBDevice) {
+
+    required init(_ deviceInfo: USBDevice) {
         self.deviceInfo = deviceInfo
     }
-    
-    func reqSync(request: UnsafeMutablePointer<IOUSBDevRequest>?) throws -> Void {
+
+    func reqSync(request: UnsafeMutablePointer<IOUSBDevRequest>?) throws {
         guard let deviceInterface = self.deviceInfo.deviceInterfacePtrPtr?.pointee?.pointee else {
             throw EXIIDeviceError.DeviceInterfaceNotFound
         }
-        
-        let kr:Int32 = deviceInterface.DeviceRequest(self.deviceInfo.deviceInterfacePtrPtr, request)
-        
-        if (kr != kIOReturnSuccess) {
+
+        let kr: Int32 = deviceInterface.DeviceRequest(self.deviceInfo.deviceInterfacePtrPtr, request)
+
+        if kr != kIOReturnSuccess {
             throw EXIIDeviceError.RequestError(desc: String(kr))
         }
     }
-    
-    func reset() throws -> Void {
-        var request = IOUSBDevRequest(bmRequestType: EXIIReqType.ToDevice.rawValue,
-                                      bRequest: EXIIReport.Reset.rawValue,
-                                      wValue: EXIIResetMode.SoftReset.rawValue,
-                                      wIndex: 0,
-                                      wLength: 0,
-                                      pData: nil,
-                                      wLenDone: 0)
-        
+
+    func reset() throws {
+        var request = IOUSBDevRequest(
+            bmRequestType: EXIIReqType.ToDevice.rawValue,
+            bRequest: EXIIReport.Reset.rawValue,
+            wValue: EXIIResetMode.SoftReset.rawValue,
+            wIndex: 0,
+            wLength: 0,
+            pData: nil,
+            wLenDone: 0
+        )
+
         try reqSync(request: &request)
     }
-    
+
     func status() throws -> EXIIDeviceStatus {
         let length: Int = 8
         var data: [UInt8] = [UInt8](repeating: 0, count: length)
-        var request = IOUSBDevRequest(bmRequestType: EXIIReqType.WithResponse.rawValue,
-                                      bRequest: EXIIReport.ControllerStatus.rawValue,
-                                      wValue: 0,
-                                      wIndex: 0,
-                                      wLength: UInt16(length),
-                                      pData: &data,
-                                      wLenDone: UInt32(length))
-        
+        var request = IOUSBDevRequest(
+            bmRequestType: EXIIReqType.WithResponse.rawValue,
+            bRequest: EXIIReport.ControllerStatus.rawValue,
+            wValue: 0,
+            wIndex: 0,
+            wLength: UInt16(length),
+            pData: &data,
+            wLenDone: UInt32(length)
+        )
+
         try reqSync(request: &request)
-        
-        return EXIIDeviceStatus.init(
-            reportId: EXIIReport.init(rawValue: data[0])!,
-            powerOnCheckStatus: EXIIPowerOnCheckStatus.init(rawValue: data[2] << 2 & data[1])!,
-            lastCommandStatus: EXIICommandStatus.init(rawValue: data[3])!,
+
+        return EXIIDeviceStatus(
+            reportId: EXIIReport(rawValue: data[0])!,
+            powerOnCheckStatus: EXIIPowerOnCheckStatus(rawValue: data[2] << 2 & data[1])!,
+            lastCommandStatus: EXIICommandStatus(rawValue: data[3])!,
             touchStatus: data[4],
             asyncReports: UInt16(data[6] << 2 & data[5])
         )
@@ -137,11 +141,12 @@ class DeviceDaemon {
     func start() {
         let deviceDaemon = Thread(
             target: exiiDeviceMon,
-            selector:#selector(exiiDeviceMon.start),
-            object: nil)
+            selector: #selector(self.exiiDeviceMon.start),
+            object: nil
+        )
 
         deviceDaemon.start()
-        
+
         print(Utils.now(), "Daemon started...", deviceDaemon)
     }
 }
@@ -160,14 +165,15 @@ class CLI {
             self,
             selector: #selector(self.addDevice),
             name: .USBDeviceConnected,
-            object: nil)
+            object: nil
+        )
     }
-    
+
     @objc func addDevice(notification: NSNotification) {
         guard let nobj = notification.object as? NSDictionary else {
             return
         }
-        guard let deviceInfo:USBDevice = nobj["device"] as? USBDevice else {
+        guard let deviceInfo: USBDevice = nobj["device"] as? USBDevice else {
             return
         }
         print(Utils.now(), "Registering device ", deviceInfo)
@@ -180,10 +186,10 @@ class CLI {
         }
         self.devices.append(device)
     }
-    
+
     func poll() {
         while true {
-            devices.forEach { (device) in
+            self.devices.forEach { device in
                 do {
                     let res = try device.status()
                     print(Utils.now(), res)
@@ -193,10 +199,9 @@ class CLI {
             }
             usleep(1000000)
         }
-        
     }
 }
 
-DeviceDaemon.init().start()
+DeviceDaemon().start()
 
-CLI.init().poll()
+CLI().poll()
